d2390df1d477178716b5443cd8e2ddb226f4b065
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-04-11 10:35:45 -0500

    Basic background map

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function ($, L) {
+
+  var atlas = L.map('atlas').setView([20, 0], 3);
+
+  L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+	   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+     subdomains: 'abcd',
+     maxZoom: 19,
+   }).addTo(atlas);
+
+})(jQuery, L);
