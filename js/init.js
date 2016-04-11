91b72f881c3e8ea835e6a32d44aff74b35d6a1ea
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-04-11 11:24:43 -0500

    Some code prep for dynamic feeds

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@
 
   /* ------ Leaflet Setup ------ */
   var atlas = L.map('atlas').setView([20, 0], 3);
+  var earthquake;
 
   L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
@@ -9,24 +10,27 @@
      maxZoom: 19,
    }).addTo(atlas);
 
-   /* ------ USGS feed Setup ------ */
-   var feed = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'; //temp url for now
-   $.getJSON(feed, function (jsondata) {
-     var geojsonMarkerOptions = {
-       radius: 8,
-       fillColor: "#ff7800",
-       color: "#000",
-       weight: 1,
-       opacity: 1,
-       fillOpacity: 0.8
-     };
+   /* ------ Default values ------ */
+   var geojsonMarkerOptions = {
+     radius: 8,
+     fillColor: "#ff7800",
+     color: "#000",
+     weight: 1,
+     opacity: 1,
+     fillOpacity: 0.8
+   };
 
-     L.geoJson(jsondata, {
-       pointToLayer: function (feature, latlng) {
-         return L.circleMarker(latlng, geojsonMarkerOptions);
-       }
-     }).addTo(atlas);
-   });
+   /* ------ USGS feeds ------ */
+   addLayer('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'); //initial load
 
-
+   function addLayer(jsonLocation) {
+     if (earthquake) atlas.removeLayer(earthquake);
+     $.getJSON(jsonLocation, function (jsondata) {
+       earthquake = L.geoJson(jsondata, {
+         pointToLayer: function (feature, latlng) {
+           return L.circleMarker(latlng, geojsonMarkerOptions);
+         }
+       }).addTo(atlas);
+     });
+   }
 })(jQuery, L);
